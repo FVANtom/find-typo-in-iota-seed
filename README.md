108b1f0fdbffa530cf4798a433cb7ec344358b8a
Tom Vergult <fvantom@terranovita.com>
2017-12-23 21:41:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+#WARNING
+NEVER give someone you do not trust 100% your seed and never enter your seed in software you do not trust. Under the hood such software can send your seed to someone who wishes to steal your funds.
+
+## What does this tool do
+
+I have written this script to help a user on Reddit who accidentally made a typo in his seed while logging in into the wallet.
+
+He then transfered iota to the address he generated and logged out of his wallet.
+
+Because he accidentally made a typo when logging in, he lost access to his funds.
+
+This program checks the most common typing mistakes and tries to find the seeds the funds were actually transferred to.
+
+* Generating seeds with one character missing
+* Generating seeds with character typed double
+* Generating seeds with letters that look like each other replaced
+* Generating seeds with block of 2 character retyped
+* Generating seeds with 2 neighbouring characters switched
+* Generating seeds with 1 character mistyped
+
+### Who is this program useful for
+
+Anyone who transferred funds to an IOTA address of a seed similar to his own but with a typo and doesn't know the 'typo-seed'.
+
+### To use this program you will need:
+
+* Your original seed
+* The address your funds were transfered (belonging to the 'typo-seed')
+
+## How to run:
+
+    mvn clean install
+
+    java -jar target/find-typo-in-iota-seed-jar-with-dependencies.jar 
+    
+## No idea how to use this tool?
+
+If you are not able to run this tool yourself, or you need customization. I am willing to try and help you.
+You can email me at iota@terranovita.com (my name is Tom)
+
+## Make a donation
+
+If you have recovered your funds using this tool, please think of my children and make a donation ;-)
+
+My iota address: 
+
+    ELDM9DAWZVHNIQQROREDKPUJRZKMZCRKROSGQXROZCYSQJKKTRNHNEBKNRSAJXOUZBGKUFWTNTN9VKTBWYJFDAQNOD
+
+## Add new version to maven local repo
+
+    mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file \
+      -Dfile=./jota-VERSION-HERE-jar-with-dependencies.jar \
+      -DgroupId=iota.lib.java \
+      -DartifactId=jota \
+      -Dversion=VERSION \
+      -Dpackaging=jar \
+      -DlocalRepositoryPath=lib
+      
+Or you can switch to the latest iota java library
+https://github.com/iotaledger/iota.lib.java
+
+## License
+
+Copyright 2017 TerraNovita Software BVBA
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/main/java/com/terranovita/iota/FindTypoInIOTASeed.java
@@ -0,0 +1,194 @@
+package com.terranovita.iota;
+
+import jota.IotaAPI;
+import jota.dto.response.GetNewAddressResponse;
+import jota.error.ArgumentException;
+import jota.error.InvalidAddressException;
+import jota.error.InvalidSecurityLevelException;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindTypoInIOTASeed {
+
+    public static String DONATION_WALLET_ADDRESS = "ELDM9DAWZVHNIQQROREDKPUJRZKMZCRKROSGQXROZCYSQJKKTRNHNEBKNRSAJXOUZBGKUFWTNTN9VKTBWYJFDAQNOD";
+    public static String POSSIBLE_CHARS = "9ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String originalSeed;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+
+        System.out.print("Enter your original seed: ");
+        String originalSeed = scanner.next().trim();
+
+        System.out.print("Enter a wallet address holding your funds: ");
+        String addressToLookFor = scanner.next().trim();
+
+        System.out.println("Index offset (default is 0): ");
+        int indexOffset = 0;
+        try {
+            String indexOffsetString = scanner.next();
+            indexOffset = Integer.parseInt(indexOffsetString);
+        } catch (NumberFormatException ex) {
+        }
+        if (indexOffset < 0) {
+            indexOffset = 0;
+        }
+
+        FindTypoInIOTASeed seedFinder = new FindTypoInIOTASeed();
+        try {
+            seedFinder.findTypoSeed(originalSeed, addressToLookFor, indexOffset);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ArgumentException e) {
+            e.printStackTrace();
+        } catch (InvalidSecurityLevelException e) {
+            e.printStackTrace();
+        } catch (InvalidAddressException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String sanitizeSeed(String seed) {
+        seed = seed.toUpperCase();
+        String newSeed = "";
+        for (int i = 0; i < seed.length(); i++) {
+            if (("9ABCDEFGHIJKLMNOPQRSTUVWXYZ").indexOf(seed.charAt(i)) < 0) {
+                newSeed += "9";
+            } else {
+                newSeed += seed.charAt(i);
+            }
+        }
+        newSeed = newSeed.length() < 81 ? newSeed + StringUtils.repeat('9', 81 - newSeed.length()) : newSeed;
+        return newSeed;
+    }
+
+
+    private List<String> addSeedsWithRemovedCharacter1(List<String> similarSeeds) {
+        System.out.println("Generating seeds with one character missing");
+        for (int x = originalSeed.length() - 2; x > 0; x--) {
+            String newSeed = originalSeed.substring(0, x - 1) + originalSeed.substring(x, originalSeed.length()) + "9";
+            if(!similarSeeds.contains(newSeed)) {
+                similarSeeds.add(newSeed);
+            }
+        }
+        return similarSeeds;
+    }
+
+    private List<String> addSeedsWithDoubleCharacter1(List<String> similarSeeds) {
+        System.out.println("Generating seeds with character typed double");
+        for (int x = originalSeed.length() - 2; x > 0; x--) {
+            String newSeed = originalSeed.substring(0, x) + originalSeed.substring(x - 1, x) + originalSeed.substring(x, originalSeed.length() - 1);
+            if(!similarSeeds.contains(newSeed)) {
+                similarSeeds.add(newSeed);
+            }
+        }
+        return similarSeeds;
+    }
+
+
+    private List<String> addSeedsWithReplacingChars(List<String> similarSeeds) {
+        System.out.println("Generating seeds with letters that look like each other replaced");
+        similarSeeds.add(originalSeed.replace('U', 'V'));
+        similarSeeds.add(originalSeed.replace('V', 'U'));
+        similarSeeds.add(originalSeed.replace('9', 'G'));
+        similarSeeds.add(originalSeed.replace('I', 'L'));
+        similarSeeds.add(originalSeed.replace('L', 'I'));
+        similarSeeds.add(originalSeed.replace('D', 'O'));
+        similarSeeds.add(originalSeed.replace('O', 'D'));
+        return similarSeeds;
+    }
+
+    private List<String> addSeedsWithDoubleCharacter2(List<String> similarSeeds) {
+        System.out.println("Generating seeds with block of 2 characters retyped");
+        for (int x = originalSeed.length() - 2; x > 1; x--) {
+            String newSeed = originalSeed.substring(0, x) + originalSeed.substring(x - 2, x) + originalSeed.substring(x, originalSeed.length() - 2);
+            if(!similarSeeds.contains(newSeed)) {
+                similarSeeds.add(newSeed);
+            }
+        }
+        return similarSeeds;
+    }
+
+    private List<String> addSeedsWithGetListOfDyslexiaSeeds(List<String> similarSeeds) {
+        System.out.println("Generating seeds with 2 neighbouring characters switched");
+        for (int x = originalSeed.length() - 2; x > 1; x--) {
+            String newSeed = originalSeed.substring(0, x - 2) + originalSeed.substring(x - 1, x) + originalSeed.substring(x - 2, x - 1) + originalSeed.substring(x);
+            if(!similarSeeds.contains(newSeed)) {
+                similarSeeds.add(newSeed);
+            }
+        }
+        return similarSeeds;
+    }
+
+    private List<String> addSeedsWithGetListOfSimilarSeeds(List<String> similarSeeds) {
+        System.out.println("Generating seeds with 1 character mistyped");
+        return innerAddSeedsWithGetListOfSimilarSeeds(similarSeeds, this.originalSeed);
+    }
+
+    private List<String> innerAddSeedsWithGetListOfSimilarSeeds(List<String> similarSeeds, String theSeed) {
+        for (int x = theSeed.length() - 3; x > 0; x--) {
+            for (int y = 0; y < POSSIBLE_CHARS.length(); y++) {
+                String newSeed = theSeed.substring(0, x) + POSSIBLE_CHARS.substring(y, y + 1) + theSeed.substring(x + 1);
+                if(!similarSeeds.contains(newSeed)) {
+                    similarSeeds.add(newSeed);
+                }
+            }
+        }
+        return similarSeeds;
+    }
+
+    /*
+    // This takes too long and is not recommended. Contact me at iota@terranovita.com if you need customized help. I'll see what I can do for you.
+    private List<String> getListOfSimilarSeeds2Chars(List<String> similarSeeds) {
+        System.out.println("Generating seeds with 2 characters mistyped (CAUTION: This will take a long time, not recommended)");
+        for (int x = originalSeed.length() - 3; x > 0 ; x--) {
+            for (int y = 0; y < POSSIBLE_CHARS.length(); y++) {
+                String tempSeed = originalSeed.substring(0, x) + POSSIBLE_CHARS.substring(y, y + 1) + originalSeed.substring(x+1);
+                innerAddSeedsWithGetListOfSimilarSeeds(similarSeeds, tempSeed);
+            }
+        }
+        return similarSeeds;
+    }*/
+
+    public void findTypoSeed(String originalSeed, String addressToLookFor, int indexOffset) throws ArgumentException, InterruptedException, InvalidAddressException, InvalidSecurityLevelException {
+
+        this.originalSeed = sanitizeSeed(originalSeed);
+
+        System.out.println("Setting up iota API");
+        IotaAPI api = new IotaAPI.Builder().build();
+
+        System.out.println("Generating all possible combinations");
+        List<String> similarSeeds = new ArrayList<String>();
+        addSeedsWithRemovedCharacter1(similarSeeds);
+        addSeedsWithDoubleCharacter1(similarSeeds);
+        addSeedsWithReplacingChars(similarSeeds);
+        addSeedsWithDoubleCharacter2(similarSeeds);
+        addSeedsWithGetListOfDyslexiaSeeds(similarSeeds);
+        addSeedsWithGetListOfSimilarSeeds(similarSeeds);
+
+        System.out.println("Starting search (Checking " + similarSeeds.size() + " seeds)");
+        for (int x = indexOffset; x < similarSeeds.size(); x++) {
+            System.out.println("checking seed with index " + x + " (" + similarSeeds.get(x) + ")");
+            GetNewAddressResponse addressResponse = api.getNewAddress(similarSeeds.get(x), 2, 0, true, 30, true);
+            if (addressResponse.getAddresses().contains(addressToLookFor)) {
+                System.out.println("");
+                System.out.println("*****************************************************************************************************************");
+                System.out.println("*                                                                                                               *");
+                System.out.println("*  WALLET SEED FOUND!!!!!: " + similarSeeds.get(x) + "    *");
+                System.out.println("*                                                                                                               *");
+                System.out.println("*****************************************************************************************************************");
+                System.out.println("*                                                                                                               *");
+                System.out.println("*  If you have recovered your funds using this tool, please think of my children and make a donation ;-)        *");
+                System.out.println("*  My iota address: " + DONATION_WALLET_ADDRESS + "  *");
+                System.out.println("*                                                                                                               *");
+                System.out.println("*****************************************************************************************************************");
+                return;
+            }
+            Thread.sleep(10l);
+        }
+    }
+}
